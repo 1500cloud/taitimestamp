108932c4437cd1b1ab7b2f93ee9681ece81f2710
Chris Northwood <chris@pling.org.uk>
2022-10-26 21:42:37 +0100

    update all dev dependencies

--- lib/serialisers.ts
+++ lib/serialisers.ts
@@ -8,6 +8,6 @@ export function taiTimestampToMediaTimestamp(ts: TaiTimestamp): string {
   return `${ts.seconds}:${ts.nanosecs.toString().padStart(9, "0")}`;
 }
 
-export function taiTimestampToNanoseconds(ts: TaiTimestamp): BigInt {
+export function taiTimestampToNanoseconds(ts: TaiTimestamp): bigint {
   return BigInt(ts.seconds) * 1000000000n + BigInt(ts.nanosecs);
 }
